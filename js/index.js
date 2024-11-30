84032d6670b60ad91ea397b5531e22bec030e1bf
Zach-Bushell <zachbushell11@gmail.com>
2024-11-30 11:49:23 -0700

    i think there's something seriously wrong with the way im approaching this

--- js/index.js
+++ js/index.js
@@ -1,41 +1,49 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     
-    
+    //fetch data needed 
     const select = document.querySelector("select");
     year = select.addEventListener("change", (e) =>{
       
         //fetch all the stuff we need. 
-       const races = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/races.php?season="+ e.target.value);
-       const quali = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?season="+ e.target.value);
-       const results = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/results.php?season="+ e.target.value);
+        let races, quali, results;
+
+        
 
        
-       quali.then(data =>{console.dir(data)});
-       results.then(data =>{console.dir(data)});
-       races.then(data =>{console.dir(data)});
+        races = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/races.php?season="+ e.target.value);
+        quali = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?season="+ e.target.value);
+        results = getApiData("https://www.randyconnolly.com/funwebdev/3rd/api/f1/results.php?season="+ e.target.value);
+ 
 
     });
    
+    //local storage handler
+    function getData(promise, year, data){
+        let v1 = promise;
+        let localCopy = localStorage.getItem(year + data);
+        if(localCopy){
+        
+        }
 
 
-
+        return raceData;
+    }
 
 
     // handy dandy reusable api getter
     function getApiData(url){
-        const apiFetch = fetch(url)
+        return fetch(url)
             .then(response =>{
                 if(response.ok){
-                    return response.json()
+                    return response.json();
                 }else{
                     console.dir(response);
-                }})
+            }})
             .catch(error =>{
                 console.dir(error);
                 throw error;
             });
-        return apiFetch;
     }
 
 
