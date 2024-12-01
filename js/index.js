6630d220ca7e68177704a72fdbe4ba91f39b1247
Zach-Bushell <zachbushell11@gmail.com>
2024-12-01 00:04:51 -0700

    added local storage back. started on qualifying handler

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 
 const URLS = [
-    "https://www/randyconnolly.com/funwebdev/3rd/api/f1/races.php?season=",
-    "https://www/randyconnolly.com/funwebdev/3rd/api/f1/results.php?season=",
-    "https://www/randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?season="
+    "https://www.randyconnolly.com/funwebdev/3rd/api/f1/races.php?season=",
+    "https://www.randyconnolly.com/funwebdev/3rd/api/f1/qualifying.php?season=",
+    "https://www.randyconnolly.com/funwebdev/3rd/api/f1/results.php?season="
+    
     ];
 
  //helper function for get data
@@ -25,14 +26,44 @@ function saveLocalCopy(key, data){
 
 }
 
-async function getAPIdata() {
+async function getAPIData(url, year) {
     try {
-        const response = await fetch(url + season);
+        const response = await fetch(url + year);
         const data = await response.json();
         return data;
      } catch (error) {
          console.dir(error);
      }
+}
+
+//gets data from promises and returns something useful. 
+//also stores data locally if not there already.
+async function getDataFromPromise(url, year){
+    const key = year + "-key"; 
+    const cachedData = getLocalCopy(key);
+
+    if (cachedData) {
+        
+        //return
+        const { rTbl, qTbl, eTbl } = cachedData;
+        return { rTbl, qTbl, eTbl };
+    } else {
+    
+        try {
+            const results = url.map(urls => getAPIData(urls, year));
+            const data = await Promise.all(results);
+            const [rTbl, qTbl, eTbl] = data;
+        
+            // Save the fetched data to localStorage
+            saveLocalCopy(key, { rTbl, qTbl, eTbl });
+            
+            return {rTbl, qTbl, eTbl};
+        
+        } catch (error) {
+            console.dir(error);
+            throw error;
+        }
+    }
 }
 
 /*
@@ -44,16 +75,40 @@ async function getAPIdata() {
 document.addEventListener("DOMContentLoaded", function(){
 
     document.querySelector("#selectYear").addEventListener("change", e=>{
-    
-    //const rTbl = document.querySelector("#raceTbl");
-    //const qTbl = document.querySelector("#qualiTbl");
-    //const eTbl = document.querySelector("#resultTbl");
+        
+        const seasonRaces = document.querySelector("#raceTbl");
+      
+
+        getDataFromPromise(URLS,e.target.value).then(({rTbl, qTbl, eTbl}) =>{
+            
+            //build the races list.
+            rTbl.forEach(e =>{
+                let c1 = document.createElement("tr");
+                c1.innerHTML = '<td>'+e.round+'</td><td>'+e.name+'</td><td><button id="'+e.round+'">Results</button></td>';
+                seasonRaces.appendChild(c1);
+
+            });
+            
+        });
+
+    //select statement bracket set
+    });
+
+    //build the rest of the race results
+    document.querySelector("#raceTbl").addEventListener("click", e =>{
+        const seasonQuali = document.querySelector("#qualiTbl");
+        const seasonResult = document.querySelector("#resultTbl");
+        
+        //make sure it's actually a button
+        if(e.target.tagName === 'BUTTON'){
+            const round = e.target.id;
 
 
+        }
+
+    //slect statement brackets
+    });    
 
 
-    
-//select statement bracket set
-    });        
 //dom bracket set
 });   
